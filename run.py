8e2bea52df08601079d4b3a9ab998a940f54e2e3
devfer-sys <franklin333f@gmail.com>
2024-12-10 14:13:25 -0400

    Update run.py

--- run.py
+++ run.py
@@ -28,5 +28,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Crear tablas si no existen
         insertar_empleados(app)  
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
